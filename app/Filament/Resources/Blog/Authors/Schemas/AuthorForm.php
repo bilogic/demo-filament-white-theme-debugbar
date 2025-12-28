0a080dbb2440554488f0bdb29a13574129db17e9
bilogic <946010+bilogic@users.noreply.github.com>
2025-12-28 13:24:03 +0800

    FileUpload won't show existing files

--- app/Filament/Resources/Blog/Authors/Schemas/AuthorForm.php
+++ app/Filament/Resources/Blog/Authors/Schemas/AuthorForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Blog\Authors\Schemas;
 
 use App\Models\Blog\Author;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
@@ -31,8 +32,11 @@ class AuthorForm
                     ->label('GitHub handle')
                     ->maxLength(255),
 
-                TextInput::make('twitter_handle')
-                    ->maxLength(255),
+                FileUpload::make('twitter_handle')
+                    ->disk('public')
+                    ->visibility('public')
+                    ->avatar(),
+                // ->maxLength(255),
             ]);
     }
 }
